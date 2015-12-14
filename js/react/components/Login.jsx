5126ff38b521dfeb54c4c0a88cad4b2c023d93fb
ebenpack <ebenpackwood@gmail.com>
2015-12-14 17:48:19 -0500

    Update Login

--- js/react/components/Login.jsx
+++ js/react/components/Login.jsx
@@ -8,17 +8,39 @@ var App = React.createClass({
             email: '',
             pass: '',
             loginOpen: false,
+            loginWaiting: false,
+            warn: false,
         };
     },
     handleEmailChange: function(event) {
         this.setState({
-            email: event.target.value
+            email: event.target.value,
+            warn: false,
         });
     },
     handlePassChange: function(event) {
         this.setState({
-            pass: event.target.value
+            pass: event.target.value,
+            warn: false,
         });
+    },
+    componentWillReceiveProps: function(nextProps) {
+        if (nextProps.authData === null) {
+            if (this.state.openLogin && this.state.loginWaiting) {
+                this.setState({
+                    loginWaiting: false,
+                    warn: true,
+                });
+            }
+        } else if (nextProps.authData) {
+            this.setState({
+                email: '',
+                pass: '',
+                openLogin: false,
+                loginWaiting: false,
+                warn: false,
+            });
+        }
     },
     submitLogin: function() {
         AppActions.submitLogin(
@@ -26,29 +48,56 @@ var App = React.createClass({
             this.state.pass
         );
         this.setState({
-            email: '',
-            pass: ''
+            loginWaiting: true,
         });
     },
-    openLogin: function(){
-        this.setState({openLogin: true});
+    closeLogin: function(){
+        this.setState({
+            openLogin: false,
+        });
     },
-    logout: function(){
+    openLogin: function() {
+        this.setState({
+            openLogin: true,
+        });
+    },
+    logout: function() {
         AppActions.logout();
     },
     render: function() {
+        var inputClasses = this.state.warn ? 'warning' : '';
+        var modal = (this.state.openLogin) ? (
+            <div className="modal" id="modal-one" aria-hidden="true">
+                <div className="modal-dialog">
+                    <div className="modal-header">
+                        <h2>LOGIN</h2>
+                        <button className="btn-close" aria-hidden="true" onClick={this.closeLogin}>×</button>
+                    </div>
+                    <div className="modal-body">
+                        <div>
+                            <label htmlFor="loginEmail">Email</label><br />
+                            <input className={inputClasses} id="loginEmail" type="text" value={this.state.email} onChange={this.handleEmailChange} />
+                        </div>
+                        <div>
+                            <label htmlFor="loginPass">Password</label><br />
+                            <input className={inputClasses} id="loginPass" type="password" value={this.state.pass} onChange={this.handlePassChange} />
+                        </div>
+                        <button onClick={this.submitLogin}>Submit</button>
+                    </div>
+                </div>
+            </div>
+        ) : '';
+
         var login = (
             (this.props.authData) ?
-            <button onClick={this.logout}>Logout</button>:
-                (!this.state.openLogin) ?
-                <button onClick={this.openLogin}>Login</button> :
-                <div>
-                    <input type="text" value={this.state.email} onChange={this.handleEmailChange} />
-                    <input type="text" value={this.state.pass} onChange={this.handlePassChange} />
-                    <button onClick={this.submitLogin}>Submit</button>
-                </div>
+            (<div>
+                    <button onClick={this.logout}>Logout</button>
+                </div>) :
+            (<div>
+                    <button onClick={this.openLogin}>Login</button>
+                    {modal}
+                </div>)
         );
-
         return login;
     }
 });
