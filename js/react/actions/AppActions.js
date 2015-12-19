b2c20834f4ec92e13cbdd073e833ed64bc8f6915
ebenpack <ebenpackwood@gmail.com>
2015-12-19 09:35:00 -0500

    Add search/remove actions

--- js/react/actions/AppActions.js
+++ js/react/actions/AppActions.js
@@ -3,9 +3,12 @@ var Reflux = require('reflux');
 var AppActions = Reflux.createActions([
     'addHistory',
     'addUpcoming',
+    'removeHistory',
+    'removeUpcoming',
     'getAuth',
     'submitLogin',
     'logout',
+    'searchMovies',
 ]);
 
 module.exports = AppActions;
