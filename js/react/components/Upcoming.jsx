0f45d97192a9d709b044f851ffcb9ff7365f8a81
ebenpack <ebenpackwood@gmail.com>
2015-12-20 13:27:30 -0500

    Add remove button

--- js/react/components/Upcoming.jsx
+++ js/react/components/Upcoming.jsx
@@ -1,23 +1,33 @@
 var React = require('react');
 var Reflux = require('reflux');
 var AppStore = require('../stores/AppStore');
+var AppActions = require('../actions/AppActions');
+var Movie = require('./Movie.jsx');
+
 
 var Upcoming = React.createClass({
     mixins: [
         Reflux.connect(AppStore, 'upcomingData')
     ],
+    handleClick: function(id, e){
+        AppActions.removeUpcoming(id);
+    },
     render: function() {
         return (
             <div>
                 {this.state.upcomingData.upcoming.map(function(movie){
                     return (
-                        <Movie
-                            title={movie.title}
-                            year={movie.year}
-                            img={movie.img}
-                        />
+                        <div>
+                            <Movie
+                                key={movie.id}
+                                title={movie.title}
+                                year={movie.year}
+                                img={movie.img}
+                            />
+                            <button onClick={this.handleClick.bind(this, movie.id)}>Remove</button>
+                        </div>
                     );
-                })}
+                }, this)}
             </div>
         );
     }
