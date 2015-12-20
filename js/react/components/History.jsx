d2103eba4f0fb367aba79bb3b4ec5d74307ac00e
ebenpack <ebenpackwood@gmail.com>
2015-12-20 13:27:38 -0500

    Add remove button

--- js/react/components/History.jsx
+++ js/react/components/History.jsx
@@ -1,24 +1,32 @@
 var React = require('react');
 var Reflux = require('reflux');
 var AppStore = require('../stores/AppStore');
+var AppActions = require('../actions/AppActions');
 var Movie = require('./Movie.jsx');
 
 var History = React.createClass({
     mixins: [
         Reflux.connect(AppStore, 'historyData')
     ],
+    handleClick: function(id, e){
+        AppActions.removeHistory(id);
+    },
     render: function() {
         return (
             <div>
                 {this.state.historyData.history.map(function(movie){
                     return (
-                        <Movie
-                            title={movie.title}
-                            year={movie.year}
-                            img={movie.img}
-                        />
+                        <div>
+                            <Movie
+                                key={movie.id}
+                                title={movie.title}
+                                year={movie.year}
+                                img={movie.img}
+                            />
+                            <button onClick={this.handleClick.bind(this, movie.id)}>Remove</button>
+                        </div>
                     );
-                })}
+                }, this)}
             </div>
         );
     }
